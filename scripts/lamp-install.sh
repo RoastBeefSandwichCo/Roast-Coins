da5538048f06164d0351131dd8b8bb90acf660ce
Sid X Brooks <ninobrooks@gmail.com>
2016-02-15 15:14:50 -0600

    fiddling with mysql install/configure

--- scripts/lamp-install.sh
+++ scripts/lamp-install.sh
@@ -7,12 +7,25 @@
 
 #pretty this up after testing
 
-MYSQL_ROOT_PW=< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-16};echo
-echo "mysqlrootpw:$MYSQL_ROOT_PW"
+randpw(){ < /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-16};echo;}
+MYSQL_ROOT_PW=`randpw 20`
+echo "MYSQL_ROOT_PW:$MYSQL_ROOT_PW"
 
-
-##echo mysql-server mysql-server/root_password password $MYSQL_ROOT_PW | sudo debconf-set-selections
-##echo mysql-server mysql-server/root_password_again password $MYSQL_ROOT_PW | sudo debconf-set-selections
+echo mysql-server mysql-server/root_password password $MYSQL_ROOT_PW | sudo debconf-set-selections
+echo mysql-server mysql -server/root_password_again password $MYSQL_ROOT_PW | sudo debconf-set-selections
 #On Debian, you must install the LAMP components manually, but for mysql only:
-##sudo apt-get install mysql-server mysql-client -y
+sudo apt-get install mysql-server mysql-client -y
 #sudo apt-get install lamp -y
+
+#install Roast Coins user.
+#@'localhost' is used because this is automated. Advanced users working with remote db don't need this script.
+MYSQL_ROAST_COINS_PW=`randpw 20`
+echo "MYSQL_ROAST_COINS_PW:$MYSQL_ROAST_COINS_PW"
+echo mysql -u root -p$MYSQL_ROOT_PW -e "CREATE USER 'roast_coins'@'localhost' IDENTIFIED BY '$MYSQL_ROAST_COINS_PW';"
+echo mysql -u root -p$MYSQL_ROOT_PW -e "CREATE DATABASE roast_coins;"
+echo mysql -u root -p$MYSQL_ROOT_PW -e "GRANT ALL PRIVILEGES ON roast_coins.* TO roast_coins@'localhost';"
+
+cp config/config.example.json config/config.json
+echo "mysql roast_coins user password is:$MYSQL_ROAST_COINS_PW" > config/PASSWORD.TXT
+echo "edit config/config.json with appropriate database settings"
+echo "user password is in config/password.txt"
--- scripts/roast-coins-mysql-init.sh
+++ scripts/roast-coins-mysql-init.sh
@@ -3,9 +3,9 @@
 #run like so: ./setup.sh
 
 
-mysql -u root -p$MYSQL_ROOT_PW -e "CREATE USER 'test01'@'localhost' IDENTIFIED BY 'test01';"
-mysql -u root -p$MYSQL_ROOT_PW -e "CREATE DATABASE test01;"
-mysql -u root -p$MYSQL_ROOT_PW -e "GRANT ALL PRIVILEGES ON test01.* TO test01@'localhost';"
+echo mysql -u root -p$MYSQL_ROOT_PW -e "CREATE USER 'test01'@'localhost' IDENTIFIED BY 'test01';"
+echo mysql -u root -p$MYSQL_ROOT_PW -e "CREATE DATABASE test01;"
+echo mysql -u root -p$MYSQL_ROOT_PW -e "GRANT ALL PRIVILEGES ON test01.* TO test01@'localhost';"
 echo "NOW CHANGE THE MYSQL ROOT PASSWORD"
 echo "NOW CHANGE THE MYSQL ROOT PASSWORD"
 echo "NOW CHANGE THE MYSQL ROOT PASSWORD"
