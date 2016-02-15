f0cf1b553ae4c789503523f0097324a92c085391
Sid X Brooks <ninobrooks@gmail.com>
2016-02-15 14:28:41 -0600

    some script fixes, still testing

--- scripts/lamp-install.sh
+++ scripts/lamp-install.sh
@@ -6,13 +6,13 @@
 #No you wont' know the password. But you obviously have root; you can reset it later
 
 #pretty this up after testing
-randpw(){ < /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-16};echo;}
-mysqlrootpw=`randpw 20`
+
+MYSQL_ROOT_PW=< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-16};echo
 echo "mysqlrootpw:$MYSQL_ROOT_PW"
 
 
-echo mysql-server mysql-server/root_password password $MYSQL_ROOT_PW | sudo debconf-set-selections
-echo mysql-server mysql-server/root_password_again password $MYSQL_ROOT_PW | sudo debconf-set-selections
+##echo mysql-server mysql-server/root_password password $MYSQL_ROOT_PW | sudo debconf-set-selections
+##echo mysql-server mysql-server/root_password_again password $MYSQL_ROOT_PW | sudo debconf-set-selections
 #On Debian, you must install the LAMP components manually, but for mysql only:
-sudo apt-get install mysql-server mysql-client -y
+##sudo apt-get install mysql-server mysql-client -y
 #sudo apt-get install lamp -y
