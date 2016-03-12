b30274f88ca15fea0c34e9844a8a29ea268cd3bf
Sid Brooks <ninobrooks@gmail.com>
2016-03-12 15:45:04 -0600

    rename bin/main.js to roast-coins.js

--- bin/roast-coins.js
+++ bin/roast-coins.js
@@ -1,16 +1,37 @@
-//THE MAIN MODULE
-//#TODO: how shutdown api?
-//#TODO: when shutdown db?
+const withdrawalPollingInterval = 10 // Multiplied by 1000 = poll db every x seconds
 
-//open database, provide connection pool
-//start api
+var api = require ('../lib/api.js') //atm only provides new block notification (deposits)
+var coinDaemons = require('../lib/coin-daemons.js'); //daemon pool and methods
+var consoleDebugLevel = 'debug';
+var logfileDebugLevel = 'debug';
+var winston = require('winston');
 
-var database = require('./models/database.js').knex;  //The db connection pool
-var api = require('./lib/api.js');
+var logger = new winston.Logger({
+    transports: [
+        new winston.transports.Console({
+            level: consoleDebugLevel,
+            colorize: true
+        }),
+        new winston.transports.File ({
+            filename: 'rc_debug.log',
+            level: logfileDebugLevel,
+            json: true
+        })
+    ]
+})
+
+var database = require('../models/database.js').main(logger); //all our abstractions
 
 function main() {
-    api.start(database);
+    logger.log('info', 'main started');
+    logger.log('verbose', 'getting pool');
+    var coinDaemonPool = coinDaemons.getDaemonPool(logger);
+//    logger.log('verbose', 'starting withdrawal manager, interval: %i', withdrawalPollingInterval);
+    logger.log('verbose', 'NOT starting withdrawal manager, interval: %j sec', withdrawalPollingInterval);
+//    var withdrawalManager = setInterval(handlePendingWithdrawals, withdrawalPollingInterval * 1000);
+    logger.log('verbose', 'starting deposit manager (api), interval: '+ '');
+    var depositManager = api.start(database, coinDaemonPool, logger);
 }
 
-
 main();
+
